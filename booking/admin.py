56a1dab545b0bbc6c374dcd14add0718c6625191
lzbhaha123 <845381951@qq.com>
2022-03-30 14:06:32 +1300

    2022-03-30

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student
+from .models import Student,Feature,FeatureList,FeatureListToFeature
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'student_name', 'student_email')
+
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Feature)
+admin.site.register(FeatureList)
+admin.site.register(FeatureListToFeature)
--- booking/models.py
+++ booking/models.py
@@ -3,7 +3,26 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    student_id = models.CharField(max_length=12)
+    student_id = models.CharField(primary_key=True,max_length=12)
     student_name = models.CharField(max_length=50)
     student_email = models.CharField(max_length=50)
     student_password= models.CharField(max_length=25)
+    def __str__(self):
+        return self.student_id
+    
+class Feature(models.Model):
+    feature_id = models.AutoField(primary_key=True)
+    name =  models.CharField(max_length=25)
+    def __str__(self):
+        return self.name
+    
+class FeatureList(models.Model):
+    feature_list_id = models.AutoField(primary_key=True)
+    name =  models.CharField(max_length=25)
+    def __str__(self):
+        return self.name
+    
+class FeatureListToFeature(models.Model):
+    feature_list =  models.ForeignKey(FeatureList,on_delete=models.CASCADE)
+    feature = models.ForeignKey(Feature,on_delete=models.CASCADE)
+    feature_count = models.IntegerField(max_length=25)
